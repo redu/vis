57ae1fe89541b4538e250c3108ff38963799ac21
fltiago <fltiago@gmail.com>
2012-03-05 17:41:18 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vis::Application.config.secret_token = '6648cf819edc7d87b6e5d9c0fc797c0a88c4b7e53b9fa82761fe7c662ec9d5f04e541e2ed132ea50d009d99f472e960f181aefe7f8e8f46c511d55392a37fe95'

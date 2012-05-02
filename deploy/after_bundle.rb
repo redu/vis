ab0749d6105fd19e75e9ce24d41482d46ad41c26
fltiago <fltiago@gmail.com>
2012-05-02 11:15:35 -0300

    Alterando o deploy hook para startat o mongolicious sem travar o terminal

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,3 +1,3 @@
 if environment.include?("production") or environment.include?("staging")
-  run "nohup ey_bundler_binstubs/mongolicious config/jobs.yml &"
+  run "nohup mongolicious config/jobs.yml 2>/dev/null 1>/dev/null &"
 end

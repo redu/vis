87dddafc693baeba7973835c20c6e5a14cd816be
Jéssica Barbalho <jessica@redu.com.br>
2012-05-11 10:46:04 -0300

    Abilitando actions a retornarem parametro callback

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,13 +4,19 @@ class SubjectsController < ApplicationController
     @activity = SubjectParticipation.new(params[:subject_id])
     @activity.extend(SubjectParticipationRepresenter)
 
-    respond_with(@activity)
+    respond_to do |format|
+      format.json { render :json => @activity,
+                    :callback => params[:callback] }
+    end
   end
 
   def activities_d3
     @d3 = SubjectParticipation.new(params[:subject_id])
     @d3.extend(SubjectParticipationD3Representer)
 
-    respond_with([@d3])
+    respond_to do |format|
+      format.json { render :json => [@d3],
+                    :callback => params[:callback] }
+    end
   end
 end

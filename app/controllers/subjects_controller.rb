af47b1ec1585b8fb089e88dcfadc3c4134b6e87e
Jéssica Barbalho <jessica@redu.com.br>
2012-07-26 08:22:33 -0300

    Removendo debugger

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,10 +1,8 @@
 class SubjectsController < ApplicationController
-
   def activities
     activity = SubjectParticipation.new(params[:subject_id])
     activity.extend(SubjectParticipationRepresenter)
 
-    debugger
     respond_to do |format|
       format.json { render :json => activity,
                     :callback => params[:callback] }

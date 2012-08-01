13acddb0e6f90ce20ed36c403f2562b3bd800a9f
Jéssica Barbalho <jessica@redu.com.br>
2012-08-01 11:59:34 -0300

    controller de subject participation

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,15 +8,4 @@ class SubjectsController < ApplicationController
                     :callback => params[:callback] }
     end
   end
-
-  def activities_d3
-    d3 = SubjectParticipation.new(params[:subject_id])
-    d3.extend(SubjectParticipationD3Representer)
-
-    respond_to do |format|
-      format.json { render :json => [d3],
-                    :callback => params[:callback] }
-    end
-  end
-
 end
